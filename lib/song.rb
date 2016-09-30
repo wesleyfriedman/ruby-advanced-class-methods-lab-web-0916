6367fabc476f150091997199b36c744af243c8ea
wesleyfriedman <friedman.wesley@gmail.com>
2016-09-29 23:11:46 -0400

    all but two methods

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,10 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  # def initialize(song=nil)
+  # 	@song = song
+  # end
+
   def self.all
     @@all
   end
@@ -9,5 +13,42 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+  	@@all << song = self.new
+  	song
+  end
+
+  def self.new_by_name(song_name)
+  	song = self.new
+  	song.name = song_name
+  	song
+  end
+
+  def self.create_by_name(song_name)
+  	song = self.create
+  	song.name = song_name
+  	song
+  end
+
+  def self.find_by_name(song_name)
+  	@@all.detect {|song| song_name == song.name }
+  end
+
+  def self.find_or_create_by_name(song_name)
+  	song = self.find_by_name(song_name)
+  	return song if song
+  	self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+  	@@all.sort_by {|song| song.name }
+  end
+
+  def self.new_from_filename
+  	
+  end
+
+
 
 end
